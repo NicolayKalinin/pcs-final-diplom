28227993b69525110ab7d96087e7d34143038e7a
Nicolay Kalinin <nikolajk202@gmail.com>
2023-06-28 18:57:20 +0300

    Files modified

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,13 +1,33 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private PdfPage page;
+    private java.io.File pdf;
+    PdfDocument doc;
 
-    public BooleanSearchEngine(File pdfsDir) throws IOException {
+    public BooleanSearchEngine(File pdfsDir, java.io.File pdf) throws IOException {
+        doc = new PdfDocument(new PdfReader(this.pdf));
+        doc.getPage(100);
+        int i = 101;
+        var text = PdfTextExtractor.getTextFromPage(page(101));
+        var words = text.split("\\P{IsAlphabetic}+");
         // прочтите тут все pdf и сохраните нужные данные,
         // тк во время поиска сервер не должен уже читать файлы
+    }
+
+    private PdfPage page(int i) {
+        return null;
+    }
+
+    public BooleanSearchEngine(File pdfs, File var, Object pdf) {
     }
 
     @Override
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,33 @@
-import java.io.File;
-import java.util.Arrays;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
+    private static Object pdf;
 
+    public static void main(String[] args) throws Exception {
+        File var = null;
+        BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"), var, pdf);
+        System.out.println(engine.search("бизнес"));
+        Map<String, Integer> freqs = new HashMap<>(); // мапа, где ключом будет слово, а значением - частота
+        Object words = new Object();
+        
+        try (ServerSocket serverSocket = new ServerSocket(8989);) { // стартуем сервер один(!) раз
+            while (true) { // в цикле(!) принимаем подключения
+                try (
+                        Socket socket = serverSocket.accept();
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream());
+                ) {
+                    // обработка одного подключения
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Не могу стартовать сервер");
+            e.printStackTrace();
+        }
         // здесь создайте сервер, который отвечал бы на нужные запросы
         // слушать он должен порт 8989
         // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
